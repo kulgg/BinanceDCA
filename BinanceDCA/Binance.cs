13d53a44b0f94c33e80461d45a9364846a083362
JlKmn <jlnklmn1@gmail.com>
2021-09-15 01:41:35 +0200

    Enable custom cron expression per DCA buy to be automatically scheduled

---
+++ BinanceDCA/Binance.cs
@@ -0,0 +1,63 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Binance.Net;
+using Binance.Net.Enums;
+using Microsoft.Extensions.Logging;
+using Quartz;
+
+namespace BinanceDCA
+{
+    public class Binance : IJob
+    {
+        private readonly BinanceClient _client;
+        private readonly ILogger<Binance> _log;
+
+        public Binance(BinanceClient client, ILogger<Binance> log)
+        {
+            _client = client;
+            _log = log;
+        }
+
+        public async Task Execute(IJobExecutionContext context)
+        {
+            JobDataMap dataMap = context.MergedJobDataMap;
+            string buyTicker = dataMap.GetString("BuyTicker");
+            string sellTicker = dataMap.GetString("SellTicker");
+            var amountToSell = (decimal) dataMap["AmountToSell"];
+            
+            _log.LogInformation("Executing {0} DCA", buyTicker);
+
+            var callResult = await _client.Spot.Market.GetPricesAsync();
+            
+            if(!callResult.Success)
+            {
+                _log.LogError(callResult.Error?.Message);
+            }
+            else
+            {
+                var symbol = $"{buyTicker}{sellTicker}";
+                var coinData = callResult.Data.FirstOrDefault(x => x.Symbol == symbol);
+                
+                if (coinData != null)
+                {
+                    _log.LogInformation("{0} price: {1}", symbol, coinData.Price);
+                    
+                    var placeOrderResult = await _client.Spot.Order.PlaceOrderAsync(symbol, OrderSide.Buy, OrderType.Market, quoteOrderQuantity: amountToSell);
+                    
+                    if (!placeOrderResult.Success)
+                    {
+                        _log.LogError(placeOrderResult.Error?.Message);
+                    }
+                    else
+                    {
+                        _log.LogInformation("{0}: Bought {1} {2} (@ {3} {4} average)", placeOrderResult.Data.Status, placeOrderResult.Data.QuantityFilled, buyTicker, placeOrderResult.Data.AverageFillPrice, sellTicker);
+                    }
+                }
+                else
+                {
+                    _log.LogInformation("Bad Ticker: {0}", symbol);
+                }
+            }
+        }
+    }
+}
---
+++ BinanceDCA/QuartzConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using BinanceDCA.Models;
+using Microsoft.Extensions.DependencyInjection;
+using Quartz;
+
+namespace BinanceDCA
+{
+    public static class QuartzConfiguration
+    {
+        public static void Setup(IServiceCollection services, Config config)
+        {
+            services.AddQuartz(q =>
+            {
+                q.SchedulerId = "Scheduler-Core";
+                q.SchedulerName = "Cron DCA Scheduler";
+                
+                q.UseMicrosoftDependencyInjectionJobFactory();
+                q.UseSimpleTypeLoader();
+                q.UseInMemoryStore();
+
+                foreach (var buy in config.Investing.Buys)
+                {
+                    q.ScheduleJob<Binance>(trigger => trigger
+                        .WithIdentity($"{buy.BuyTicker}{buy.SellTicker}")
+                        .UsingJobData("BuyTicker", buy.BuyTicker)
+                        .UsingJobData("SellTicker", buy.SellTicker)
+                        .UsingJobData("AmountToSell", buy.AmountToSell)
+                        .StartNow()
+                        .WithSchedule(CronScheduleBuilder.CronSchedule(buy.Cron))
+                    );
+                }
+            });
+            
+            services.AddQuartzHostedService(options =>
+            {
+                options.WaitForJobsToComplete = true;
+            });
+        }
+    }
+}
